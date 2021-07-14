591d0e25b5daa95163832f035fa83624c82f19b5
eric <wyjun1988@gmail.com>
2021-07-14 23:18:39 +0900

    add layout constraint

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_study/src/basicLayout.dart';
+import 'package:flutter_study/src/layoutConstraint.dart';
 import 'package:flutter_study/src/simpleLayoutExample.dart';
 
 import 'src/basicWidget.dart';
@@ -12,32 +13,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primaryColor: Colors.purple,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('flutter layout demo'),
-        ),
-        body: ListView(
-          children: [
-            imageSection(),
-            titleSection(),
-            buttonSection(Theme.of(context).primaryColor)
-          ],
-        ),
-      )
-    );
+    return HomePage();
   }
 }
