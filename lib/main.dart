5e00122962c595080ba0b13e8359bc697f1c5bb2
eric <wyjun1988@gmail.com>
2021-07-18 20:02:56 +0900

    add state widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_study/src/basicLayout.dart';
 import 'package:flutter_study/src/layoutConstraint.dart';
 import 'package:flutter_study/src/simpleLayoutExample.dart';
+import 'package:flutter_study/src/stateWidget.dart';
 
 import 'src/basicWidget.dart';
 
@@ -13,6 +14,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return HomePage();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+        ),
+        body: Center(
+          child: ParentWidget(),
+        ),
+      ),
+    );
   }
 }
